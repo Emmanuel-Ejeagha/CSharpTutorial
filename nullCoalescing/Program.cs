3e437bfdcfbd756660d7dd1e7d245cb278bef7d7
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-01-13 15:20:45 +0100

    Add nullCoalescing

---
+++ nullCoalescing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+
+    {
+        int? TicketsOnSale = null;
+        int AvailableTickets = TicketsOnSale ?? 0;
+        Console.WriteLine("Available Tickets = {0}", AvailableTickets);
+    }
+    
+
+}
