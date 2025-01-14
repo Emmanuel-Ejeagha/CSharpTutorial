198a33dd4a28a8ab82d36a2be7129cc81d9e0c96
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-01-14 10:57:05 +0100

    Add: params " "

---
+++ ParameterArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] Numbers = new int[3];
+
+Numbers[0] = 300;
+Numbers[1] = 200;
+Numbers[2] = 78;
+
+ParamsMethod(Numbers);
+
+void ParamsMethod(params int[] Numbers)
+{
+    Console.WriteLine($"There are {Numbers.Length}");
+
+    foreach (int i in Numbers)
+    {
+        Console.WriteLine(i);
+    }
+}
