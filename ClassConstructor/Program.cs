c6827fc3fbae121954b683083fef805e2af16916
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-01-14 14:02:13 +0100

    Add: class constructor

--- ClassConstructor/Program.cs
+++ ClassConstructor/Program.cs
@@ -6,6 +6,9 @@ class Program
     {
         Customer C1 = new Customer("Emmanuel", "Ejeagha");
         C1.PrintFullName();
+
+        Customer C2 = new Customer();
+        C2.PrintFullName();
     }
 }
 
@@ -13,6 +16,12 @@ class Customer
 {
     string _firstName;
     string _lastName;
+
+    public Customer()
+        : this("No FirstName Provided", "No LastName Provided")
+    { 
+
+    }
 
     public Customer(string FirstName, string LastName)
     {
