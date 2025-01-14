54d5af416553cf19910cb966abe928c76cf3c7c7
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-01-14 13:02:24 +0100

    Add: class constructor

---
+++ ClassConstructor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Customer C1 = new Customer("Emmanuel", "Ejeagha");
+        C1.PrintFullName();
+    }
+}
+
+class Customer
+{
+    string _firstName;
+    string _lastName;
+
+    public Customer(string FirstName, string LastName)
+    {
+        this._firstName = FirstName;
+        this._lastName = LastName;
+    }
+
+    public void PrintFullName()
+    {
+        Console.WriteLine("Full Name = {0}", this._firstName + " " + this._lastName);
+    }
+
+    ~Customer()
+    {
+        // clean
+    }
+}
